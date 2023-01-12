adfda8d5c215329e111545a01dfea8c37cd8ef0b
Mace Akkari <mace.akkari@relyfe.com>
2023-01-12 17:04:57 +0000

    add game logic

---
+++ index.js
@@ -0,0 +1,51 @@
+const RESULT = {
+  WIN: 1,
+  LOSS: 0,
+};
+
+// found on mdn
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function game() {
+  return Array.from(new Array(3), () => getRandomIntInclusive(0, 2));
+}
+
+function render(reels) {
+  const target = document.querySelector("ol");
+  const fragment = document.createFragment();
+  for (const reel of reels) {
+    const li = document.createElement("li");
+    li.textContent = reel;
+    fragment.append(li);
+  }
+}
+
+function message(message) {
+  const target = document.querySelector("h2");
+  target.textContent = message;
+}
+
+function result(reels) {
+  const win = reels.every((x, _, arr) => x === arr.at(0));
+  return win ? RESULT.WIN : RESULT.LOSS;
+}
+
+function play() {
+  const reels = game();
+  switch (result(reels)) {
+    case RESULT.WIN:
+      message("WINNER WINNER WINNER!!!!!");
+      break;
+    default:
+      message("Sorry, better luck next time");
+  }
+  render(reels);
+}
+
+function main() {
+  document.querySelector("button").addEventListener("click", play);
+}
