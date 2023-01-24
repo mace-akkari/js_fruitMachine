11c450d10b49765657079ef6cf7a45d30dccc792
Mace Akkari <mace.akkari@relyfe.com>
2023-01-24 19:15:46 +0000

    add credits counter

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const RESULT = {
 
 const SYMBOLS = ["🍒", "💵", "🍇"];
 
+let balance = 5;
+
 // found on mdn
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -23,8 +25,8 @@ function render(reels) {
     const li = document.createElement("li");
     li.textContent = SYMBOLS[reel];
     fragment.append(li);
-    console.log(li);
   }
+  target.replaceChildren(fragment);
 }
 
 function message(message) {
@@ -32,25 +34,51 @@ function message(message) {
   target.textContent = message;
 }
 
+function credits(amount) {
+  const target = document.querySelector("h3");
+  target.textContent = `${amount} credits remaining`;
+} // try mdn -intl for formating the s
+
 function result(reels) {
   const win = reels.every((x, _, arr) => x === arr.at(0));
   return win ? RESULT.WIN : RESULT.LOSS;
 }
 
+function calculateBalance(amount, result) {
+  if (amount === 0) {
+    return 0;
+  }
+  switch (result) {
+    case RESULT.WIN:
+      return amount * 2;
+    case RESULT.LOSS:
+      return amount - 1;
+    default:
+      return amount;
+  }
+}
+
 function play() {
   const reels = game();
-  switch (result(reels)) {
+  const res = result(reels);
+  switch (res) {
     case RESULT.WIN:
       message("WINNER WINNER WINNER!!!!!");
       break;
     default:
       message("Sorry, better luck next time");
   }
+  balance = calculateBalance(balance, res);
   render(reels);
+  credits(balance);
+  if (balance === 0) {
+    document.querySelector("button").disabled = true;
+  }
 }
 
 function main() {
   document.querySelector("button").addEventListener("click", play);
+  credits(balance);
 }
 
 main();
